7e04e095d423aff8a782768a415e8d7ffcc3d1fe
Swati Fursule <swati@silvertrak.com.au>
2018-10-03 17:19:49 +1000

    added upload function and endpoint

--- index.js
+++ index.js
@@ -21,5 +21,42 @@ AWS.config.setPromisesDependency(bluebird);
 //create s3 instance
 const s3 = new AWS.S3();
 
+
+// abstract function to upload a file returning a promise
+const uploadFile = (buffer, name, type) =>{
+    const params = {
+        ACL: 'public-read',
+        Body: buffer,
+        Bucket: process.env.TEST_S3_BUCKET,
+        ContentType: type.mime,
+        Key: `${name}.${type.ext}`
+    };
+    return s3.upload(params).promise();
+}
+
+
+// Define POST route
+app.post('/upload-file', (request, response) => {
+    const form = new multiparty.Form();
+    form.parse(request, async(error, fields, files) =>{
+        if (error){
+            throw new Error(error);
+        }
+        try{
+            const path = files.file[0].path;
+            const buffer = fs.readFileSync(path);
+            const type = fileType(buffer);
+            const timestamp = Date.now().toString();
+            const fileName = `bucketFolder/${timestamp}-lg`;
+            const data = await uploadFile(buffer, fileName, type);
+            return response.status(200).send(data);
+        }catch(err){
+            return response.status(400).send(err);
+        }
+    });
+});
+
+
+
 app.listen(process.env.PORT || 8888);
 console.log('server is up and running');
